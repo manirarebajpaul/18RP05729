8f5ccc915873210398a26055d7074fdfa9583e97
manirarebajpaul <77941926+manirarebajpaul@users.noreply.github.com>
2021-01-25 01:32:35 +0200

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,13 @@
+class Address
+{
+	String country;
+	String city;
+	String zipcode;
+	Address(String country, String city, String zipcode)
+	{
+		this.country=country;
+		this.city=city;
+		this.zipcode=zipcode;
+	}
+
+}
